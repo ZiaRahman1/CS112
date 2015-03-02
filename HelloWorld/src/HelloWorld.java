6c477e453f6c523d63271e32e8c3557df9627452
ziarahman1 <zrahman01@gmail.com>
2015-03-01 20:46:01 -0800

    Class three changes

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,6 +4,16 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World! from Santa Ana College");
+		
+		String text = "Hello World!";
+        System.out.println("Hello");
+        System.out.println("My name is Zia Rahman");
+
+        System.out.println("Hello \nMy name is Zia Rahman");
+
+        System.out.printf("%s\n%s","Hello","My name is Zia Rahman");
+        System.out.println("\n");
+        System.out.println(text);
 
 	}
 }
