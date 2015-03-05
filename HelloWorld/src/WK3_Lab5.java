31063779871256c2fdd0a3692105f70cd8257bd2
ziarahman1 <zrahman01@gmail.com>
2015-03-04 16:33:25 -0800

    Week 3 Lab 5

---
+++ HelloWorld/src/WK3_Lab5.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class WK3_Lab5 {
+
+	public static void main (String [] args){
+		
+		Scanner read = new Scanner(System.in);
+		int num1,num2;
+		
+		System.out.printf("Please enter first number!");
+		num1 = read.nextInt();
+		
+		System.out.printf("Please enter first number!");
+		num2 = read.nextInt();
+		
+		System.out.printf("Hello you enter %d and %d.",num1,num2);
+	}
+}
