2f8be8e775a5b85a4cf25c06f374dd768c989aec
ziarahman1 <zrahman01@gmail.com>
2015-03-16 21:33:16 -0700

    Week 5 Homework

--- HelloWorld/src/GuessANumber_V2.java
+++ HelloWorld/src/GuessANumber_V2.java
@@ -10,27 +10,27 @@ public class GuessANumber_V2 {
 		int correctGuess = 0;
 		int wrongGuess = 0;
 
-		do {
+		System.out.printf("Please enter a number between 1 -- 10, OR -1 to EXIT");
+		number = read.nextInt();
+
+		while (number != -1) {
 			random = 0 + (int) (Math.random() * 10);
-			System.out.printf("Random number is %d\n.", random);
-
-			System.out.print("Please enter a number between 1 -- 10. or enter -1 to exit");
-			number = read.nextInt();
-
 			if (random == number) {
 				System.out.println("Numbers are same");
-				correctGuess+=1;
+				correctGuess += 1;
+			} else if (random > number) {
+				System.out.printf("Your number(%d) is smaller than random number(%d).\n",number, random);
+				wrongGuess += 1;
+			} else {
+				System.out.printf("Your number(%d) is larger than random number(%d).\n",number, random);
+				wrongGuess += 1;
 			}
-			if (random > number) {
-				System.out.println("Your number is smaller than random number.");
-				wrongGuess+=1;
-			}
-			if (random < number) {
-				System.out.println("Your number is larger than random number.");
-				wrongGuess+=1;
-			}
+			programCounter += 1;
+			System.out.print("\nPlease enter a number between 0 -- 10. -- OR enter -1 to exit");
+			number = read.nextInt();
+		}
 
-		} while (number != -1);
-
+		System.out.printf("\nTotal run = %d, Total Success = %d, Total Failure = %d\n",programCounter, correctGuess, wrongGuess);
+		System.out.println("Good Bye.");
 	}
 }
