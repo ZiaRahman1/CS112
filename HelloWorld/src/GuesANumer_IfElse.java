4e00fc8c91c048aa141233c5a7f2f707b641d268
ziarahman1 <zrahman01@gmail.com>
2015-03-23 14:36:54 -0700

    more comments

--- HelloWorld/src/GuesANumer_IfElse.java
+++ HelloWorld/src/GuesANumer_IfElse.java
@@ -7,7 +7,7 @@ public class GuesANumer_IfElse {
 		Scanner read = new Scanner(System.in);
 		int number, random;
 		random = 0 + (int) (Math.random() * 10);	
-			
+		//more comments	
 		System.out.print("Please enter a number between 1 -- 10. ");
 		number = read.nextInt();
 		
